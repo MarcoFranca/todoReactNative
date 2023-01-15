8c6914cfc5e12fe0070b87ad3a55df68c52f0c2c
Marco Franca <dev.marcotullio@gmail.com>
2023-01-15 08:25:50 -0300

    implementação do reducer TOGGLE_TODO

--- src/redux/reducers/todoListReducer.js
+++ src/redux/reducers/todoListReducer.js
@@ -1,12 +1,26 @@
-import {ADD_TODO} from "../actions";
+import {ADD_TODO, TOGGLE_TODO} from "../actions";
+
+let nextId = 1;
 
 const todoListReducer = (state = [], action) =>{
     switch (action.type) {
         case ADD_TODO:
             const newTodo = {
-                text: action.text
+                id:nextId++,
+                text: action.text,
+                done: false
             }
             return[...state, newTodo]
+        case TOGGLE_TODO:
+            return state.map(todo => {
+                if (todo.id === action.todoId){
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    };
+                }
+                    return todo;
+            })
         default:
             return state;
     }
