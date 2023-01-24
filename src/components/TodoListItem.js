133358d8609e6f3891d30d293ce2a0ffab5b3b05
Marco Franca <dev.marcotullio@gmail.com>
2023-01-23 22:47:06 -0300

    estilização das listas e do botão de deletar [UPDATE]

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 
-const TodoListItem = ({todo, onPressTodo, onLongPressTodo}) => (
+const TodoListItem = ({todo, onPressTodo, onLongPressTodo, deleteTodo}) => (
         <TouchableOpacity
             onPress ={onPressTodo}
             onLongPress = {onLongPressTodo}
@@ -13,6 +13,12 @@ const TodoListItem = ({todo, onPressTodo, onLongPressTodo}) => (
                 ]}>
                     {todo.text}
                 </Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={deleteTodo}
+                >
+                    <Text style={styles.buttonText}>Delete</Text>
+                </TouchableOpacity>
             </View>
         </TouchableOpacity>
     )
@@ -24,23 +30,37 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         height:60,
         elevation:4,
-        backgroundColor: '#e2f9ff',
+        backgroundColor: '#490bbd',
         alignItems: 'center',
-        // marginTop:15,
-        marginBottom:10,
+        marginLeft: 5,
+        marginRight:5,
+        marginBottom:5,
+        marginTop:5,
+        paddingRight:5,
     },
     lineText:{
         flex: 7,
         fontSize:20,
+        fontWeight:"bold",
         paddingLeft:15,
-    },
-    avatar:{
-        flex:1,
-        borderRadius:50,
-        aspectRatio:1,
-        marginLeft:15,
+        color:'#ffffff'
     },
     lineThrough:{
         textDecorationLine:"line-through",
+        textDecorationStyle: "solid",
+        color: "rgba(255,255,255,0.47)"
+    },
+    button:{
+        paddingTop: 10,
+        paddingBottom:10,
+        paddingLeft:20,
+        paddingRight:20,
+        borderRadius: 20,
+        backgroundColor: '#cc2f3f',
+    },
+    buttonText:{
+        fontSize:20,
+        fontWeight:"bold",
+        color:'#ffffff'
     }
 })
