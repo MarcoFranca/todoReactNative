9bceef8596ceecbe0d0163bfd707357d1aa92e61
Marco Franca <dev.marcotullio@gmail.com>
2023-01-23 22:46:31 -0300

    state inicial ao fazer um update [UPDATE]

--- src/redux/reducers/editingTodoReducer.js
+++ src/redux/reducers/editingTodoReducer.js
@@ -1,4 +1,4 @@
-import {ADD_TODO, SET_EDITING_TODO, SET_TODO_TEXT} from "../actions";
+import {ADD_TODO, SET_EDITING_TODO, SET_TODO_TEXT, UPDATE_TODO} from "../actions";
 
 const INITIAL_STATE = {
     id: null,
@@ -9,13 +9,15 @@ const editingTodoReducer = (state = INITIAL_STATE ,action)=>{
     switch (action.type) {
         case SET_TODO_TEXT:
             return {
-              ...state,
+                ...state,
                 text:action.text
             };
         case ADD_TODO:
+        case UPDATE_TODO:
             return INITIAL_STATE
         default:
             return state;
+
         case SET_EDITING_TODO:
             return action.todo;
     }
