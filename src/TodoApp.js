4754d6dbe5ab57d4b1d4536223070cd7b8af1d76
Marco Franca <dev.marcotullio@gmail.com>
2023-01-25 22:16:05 -0300

    Titulo e subtitulo do APP + barra de rodape em linear gradient [UPDATE]

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,26 +1,35 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import TodoForm from "./components/TodoForm";
+import {Dimensions} from 'react-native';
+import TodoForm from "./components/form/TodoForm";
 import {Provider} from "react-redux";
 import TodoList from "./components/TodoList";
 import {store} from "./redux/store/store";
+import {LinearGradient} from "expo-linear-gradient";
+import {Container, FinalBar} from "./TodoAppStyle";
 
+
+const {width} = Dimensions.get('window')
 export default class TodoApp extends Component {
     render() {
         return (
             <Provider store={store} >
-                <View style={styles.container}>
+                <Container>
+                    <Container.Title >To Do List</Container.Title>
+                    <Container.Subtitle >Organize a sua vida</Container.Subtitle>
                     <TodoForm />
                     <TodoList />
-                </View>
+                </Container>
+                <FinalBar width={width}>
+                    <LinearGradient
+                        style={{flex:1}}
+                        colors={["transparent","#000000"]}
+                        start={{x: 0.5, y: 0}}
+                        end={{x:0.5, y:1}}
+                        // locations={[.5,0.7]}
+                    />
+                </FinalBar>
             </Provider>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container:{
-        paddingTop:30,
-    }
-})
 
